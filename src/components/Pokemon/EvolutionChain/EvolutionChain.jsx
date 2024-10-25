4d15ffb7b95c91b747f6cd392ea31c65966a6350
Andres Prado Morgaz <andpramordev@gmail.com>
2024-10-25 19:17:37 +0200

    Passed the aux stringify to console to stop it from disturbing the UI for the build

--- src/components/Pokemon/EvolutionChain/EvolutionChain.jsx
+++ src/components/Pokemon/EvolutionChain/EvolutionChain.jsx
@@ -1,9 +1,5 @@
 export const EvolutionChain = ({ evolution }) => {
-// TODO: Que cada uno sea un link a pokemonDetails/:id
-
-  return (
-    <>
-      {JSON.stringify(evolution)}
-    </>
-  )
+  // TODO: Que cada uno sea un link a pokemonDetails/:id
+  console.log(JSON.stringify(evolution))
+  return <></>
 }
