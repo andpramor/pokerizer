3a2f3989ee5504bd96878fd5e8fcdde346d9a6d2
Andres Prado Morgaz <andpramordev@gmail.com>
2024-10-29 02:47:51 +0100

    onClick navigate -> Link

--- src/components/Pokemon/EvolutionChain/EvolutionChain.jsx
+++ src/components/Pokemon/EvolutionChain/EvolutionChain.jsx
@@ -1,13 +1,10 @@
 import './EvolutionChain.css'
 
 import { SPRITE_IMG } from '../../../services/constants.js'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export const EvolutionChain = ({ evolution }) => {
-  // TODO: gestionar triggers, por ahora, si no es por nivel o con item: special xD
-  const navigate = useNavigate()
-
-  const handleClick = (id) => navigate(`/pokemonDetails/${id}`)
+  // TODO: handle triggers, for now only handled level up and item use cases.
 
   return (
     <section className='evolution-container'>
@@ -15,23 +12,28 @@ export const EvolutionChain = ({ evolution }) => {
       <article className='evolution'>
         {Object.values(evolution).map((stage, index) => (
           <>
-            {index !== 0 && <i className='evolution-separator bi bi-arrow-right-square' />}
+            {index !== 0 && (
+              <i className='evolution-separator bi bi-arrow-right-square' />
+            )}
             <section key={index} className='evolution-stage'>
               {stage.map((pokemon) => (
                 <div key={pokemon.id} className='evolution-pokemon'>
                   <div className='evolution-member'>
-                    <img
-                      onClick={() => handleClick(pokemon.id)}
-                      src={`${SPRITE_IMG}${pokemon.id}.png`}
-                      alt={`${pokemon.name}'s sprite`}
-                    />
-                    <h3 onClick={() => handleClick(pokemon.id)}>
-                      #{pokemon.id}{' '}
-                      {pokemon.name
-                        .charAt(0)
-                        .toUpperCase()
-                        .concat(pokemon.name.slice(1))}
-                    </h3>
+                    <Link to={`/pokemonDetails/${pokemon.id}`}>
+                      <img
+                        src={`${SPRITE_IMG}${pokemon.id}.png`}
+                        alt={`${pokemon.name}'s sprite`}
+                      />
+                    </Link>
+                    <Link to={`/pokemonDetails/${pokemon.id}`}>
+                      <h3>
+                        #{pokemon.id}{' '}
+                        {pokemon.name
+                          .charAt(0)
+                          .toUpperCase()
+                          .concat(pokemon.name.slice(1))}
+                      </h3>
+                    </Link>
                   </div>
                   {pokemon.trigger !== 'unknown' && (
                     <div className='evolution-info'>
