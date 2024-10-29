dd335b818df42a5033cdab2a1614dea7ed2a85f2
Andres Prado Morgaz <andpramordev@gmail.com>
2024-10-29 02:52:36 +0100

    onClick navigate -> Link

--- src/components/Pokemon/PokemonCard/PokemonCard.jsx
+++ src/components/Pokemon/PokemonCard/PokemonCard.jsx
@@ -1,31 +1,29 @@
-import { useNavigate } from 'react-router-dom'
-import { SPRITE_IMG } from '../../../services/constants'
 import './PokemonCard.css'
 
+import { SPRITE_IMG } from '../../../services/constants'
+import { Link } from 'react-router-dom'
+
 export const PokemonCard = ({ pokemon }) => {
-  const navigate = useNavigate()
-  const handleClick = () => {
-    navigate(`/pokemonDetails/${pokemon.id}`)
-  }
   return (
     <article className='pokemonCard bg-blue-gradient'>
-      <h2 onClick={handleClick}>
-        #{pokemon.id} {pokemon.name}
-      </h2>
-      <img
-        onClick={handleClick}
-        src={`${SPRITE_IMG}${pokemon.id}.png`}
-        alt={`${pokemon.name} sprite`}
-        loading='lazy'
-      />
+      <Link to={`/pokemonDetails/${pokemon.id}`}>
+        <h2>
+          #{pokemon.id} {pokemon.name}
+        </h2>
+      </Link>
+      <Link to={`/pokemonDetails/${pokemon.id}`}>
+        <img
+          src={`${SPRITE_IMG}${pokemon.id}.png`}
+          alt={`${pokemon.name} sprite`}
+          loading='lazy'
+        />
+      </Link>
       <div className='pokemonCard-controls'>
         <label htmlFor='seenCheckbox'>
-          <i className='bi bi-check-square'></i>{' '}
-          Seen
+          <i className='bi bi-check-square'></i> Seen
         </label>
         <label htmlFor='capturedCheckbox'>
-        <i className='bi bi-square'></i>{' '}
-          Captured
+          <i className='bi bi-square'></i> Captured
         </label>
       </div>
     </article>
