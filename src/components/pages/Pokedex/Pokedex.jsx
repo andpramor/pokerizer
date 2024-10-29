3a183a87dd2e97cf2ba1b29f13ac1c50ba64544b
Andres Prado Morgaz <andpramordev@gmail.com>
2024-10-29 07:32:56 +0100

    Styling, added filter placeholders

--- src/components/pages/Pokedex/Pokedex.jsx
+++ src/components/pages/Pokedex/Pokedex.jsx
@@ -4,22 +4,31 @@ import { usePokemonList } from '../../../hooks/usePokemonList.js'
 import { PokemonCard } from '../../Pokemon/PokemonCard/PokemonCard.jsx'
 
 export const Pokedex = () => {
-  // TODO: Check API docs to get filters for GAMES or at least GENERATIONS.
   const { pokemonList } = usePokemonList()
 
   return (
     <div className='pokedex'>
       <h1 className='pokedex-name bg-blue-gradient'>National pokedex</h1>
-      <p style={{display: 'none'}}>Seen: x / 1025</p>
-      <p style={{display: 'none'}}>Got: y /1025</p>
-      <select>
-        <option value='0'>National pokedex (every pokemon)</option>
-        <option disabled> --- Gen I --- </option>
-        <option value='1'>Kanto (Pokémon Red, Blue, Gold, Silver and Crystal)</option>
-        <option disabled> --- Gen II --- </option>
-        <option value='3'>Johto (Pokémon Gold, Silver and Crystal)</option>
-        <option value='3'>Updated Johto (Pokémon Heart Gold and Soul Silver)</option>
-      </select>
+      <p style={{ display: 'none' }}>Seen: x / 1025. Got: y / 1025.</p>
+      {/* Filters: by type, by seen, by captured, by favourites */}
+      <label htmlFor='pokedex-selection'>
+        <span>Select a pokédex</span>{' '}
+        <select name='pokedex-selection'>
+          <option value='0'>National pokedex (every pokemon)</option>
+          <option disabled> --- Gen I --- </option>
+          <option value='1'>
+            Kanto (Pokémon Red, Blue, Gold, Silver and Crystal)
+          </option>
+          <option disabled> --- Gen II --- </option>
+          <option value='3'>Johto (Pokémon Gold, Silver and Crystal)</option>
+          <option value='3'>
+            Updated Johto (Pokémon Heart Gold and Soul Silver)
+          </option>
+        </select>
+      </label>
+      <label htmlFor='pokedex-search'>
+        <span>Search Pokémon</span> <input type='text' name='pokedex-search' />
+      </label>
       <ul className='pokedex-list'>
         {pokemonList.map((pokemon) => (
           <li key={pokemon.name}>
