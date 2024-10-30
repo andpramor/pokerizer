09f7b8f2c1d16eec4f890a21d79de2b4a4271381
Andres Prado Morgaz <andpramordev@gmail.com>
2024-10-30 16:47:29 +0100

    Pass id to number

--- src/hooks/usePokemonList.js
+++ src/hooks/usePokemonList.js
@@ -28,7 +28,7 @@ export const usePokemonList = () => {
 
               data.pokemon.forEach((pokemonEntry) => {
                 const { name, url } = pokemonEntry.pokemon
-                const id = url.match(/\/(\d+)\/$/)[1] // The id is surrounded by slashes at the end of the url attribute of the pokemon object in the API response
+                const id = Number(url.match(/\/(\d+)\/$/)[1]) // The id is surrounded by slashes at the end of the url attribute of the pokemon object in the API response
                 const slot = pokemonEntry.slot
 
                 // Initialize pokemon if it doesn't already exist in the map
@@ -46,9 +46,7 @@ export const usePokemonList = () => {
         )
 
         // Convert the map to an array of Pokémon objects
-        const newPokemonList = Array.from(pokemonMap.values()).sort((a, b) => {
-          return Number(a.id) - Number(b.id)
-        })
+        const newPokemonList = Array.from(pokemonMap.values()).sort()
         setPokemonList(newPokemonList)
         localStorage.setItem('pokemonList', JSON.stringify(newPokemonList))
       } catch (error) {
