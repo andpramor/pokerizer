5bbf62caf11d59800ccff99a7089f170ffba4387
Andres Prado Morgaz <andpramordev@gmail.com>
2024-11-03 18:48:58 +0100

    Header responsive styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import './Header.css'
 import favicon from '../../../favicon.png'
-import userPlaceholder from '../../assets/userPlaceholder.png'
 
 import { Link, useNavigate } from 'react-router-dom'
 import { Search } from '../Search/Search'
@@ -12,53 +11,41 @@ export const Header = () => {
     const randomPokemon = getRandomPokemonNumber()
     navigate(`/pokemonDetails/${randomPokemon}`)
   }
-  
+
   return (
     <header className='header'>
-      <nav aria-label='Main navigation'>
-        <ul className='header-links'>
-          <li>
-            <Link to='/'>
-              <img
-                src={favicon}
-                alt='Pokerizer logo'
-                className='pokerizer-logo'
-              />
-            </Link>
-          </li>
-          <li>
-            <Search />
-          </li>
-          <li onClick={handleRandom} className='header-link'>
-            Random Pokémon
-          </li>
-          <li>
-            <Link to='/pokedex'>Your Pokedex</Link>
-          </li>
-        </ul>
-      </nav>
-      <nav aria-label='User related navigation'>
-        <ul className='user-links'>
-          <li style={{ display: 'none' }}>
-            <Link to='/login'>Log in/out</Link>
-          </li>
-          <li style={{ display: 'none' }}>
-            <Link to='/singup'>Sign up</Link>
-          </li>
-          <li>
-            <Link to='/about'>ABOUT</Link>
-          </li>
-          <li>
-            <Link to='/profile'>
-              <img
-                src={userPlaceholder}
-                alt='User profile picture'
-                className='user-profile-pic'
-              />
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Link to='/'>
+        <img src={favicon} alt='Pokerizer logo' className='pokerizer-logo' />
+      </Link>
+      <i className='bi bi-list burger' />
+      <section className='header-navigation'>
+        <nav aria-label='Main navigation'>
+          <ul className='header-links'>
+            <li>
+              <Search />
+            </li>
+            <li onClick={handleRandom} className='header-link'>
+              Random Pokémon
+            </li>
+            <li>
+              <Link to='/pokedex'>Your Pokedex</Link>
+            </li>
+          </ul>
+        </nav>
+        <nav aria-label='About and authentication related navigation'>
+          <ul className='user-links'>
+            <li style={{ display: 'none' }}>
+              <Link to='/login'>Log in/out</Link>
+            </li>
+            <li style={{ display: 'none' }}>
+              <Link to='/singup'>Sign up</Link>
+            </li>
+            <li>
+              <Link to='/about'>ABOUT</Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
     </header>
   )
 }
